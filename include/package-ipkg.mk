0f459b2e94c879f1aba3ce08cf22e4ee6c76e234
Paul Spooren <mail@aparcar.org>
2020-07-15 14:20:18 -1000

    build: store buildsystem revision in packages
    In the past buildinfo files where added to enable testing for
    reproducible builds. The combination of the three buildinfo files
    feeds.buildinfo, config.buildinfo and version.buildinfo allow to
    recreate the buildsystem and check if compiled binaries are bit for bit
    the same as distributed on official servers.
    
    To support the verification efforts of the reproducible-builds.org team
    this patch adds the buildsystem revision to the package index. This way
    external feeds all external feeds can be rebuild and verified.
    
    Instead of adding a single file to the Packages folder containing the
    revision, the per package storage of revisions allow iterative updates.
    Even though the buildbots currently rebuild all packages, it would also
    be possible to only rebuild upgraded packages in the future.
    
    To keep the `Packages` small, it is ignored when creating the index.
    
    Tested with buildsystem and SDK on ath79/generic.
    
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- include/package-ipkg.mk
+++ include/package-ipkg.mk
@@ -184,6 +184,7 @@ $$(call addfield,Depends,$$(Package/$(1)/DEPENDS)
 )$$(call addfield,License,$(LICENSE)
 )$$(call addfield,LicenseFiles,$(LICENSE_FILES)
 )$$(call addfield,Section,$(SECTION)
+)$$(call addfield,BuildsystemRevision,$(REVISION)
 )$$(call addfield,Require-User,$(USERID)
 )$(if $(PKG_CPE_ID),CPE-ID: $(PKG_CPE_ID)
 )$(if $(filter hold,$(PKG_FLAGS)),Status: unknown hold not-installed
