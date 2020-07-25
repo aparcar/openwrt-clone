1cd2e78a9a62a671a4a97c25786e3712348ceb2e
Todor Colov <todorcolov@abv.bg>
2020-07-25 19:21:03 +0300

    ipq806x: Add support for Linksys E8350-v1
    Signed-off-by: Todor Colov <todorcolov@abv.bg>

--- target/linux/ipq806x/base-files/lib/upgrade/platform.sh
+++ target/linux/ipq806x/base-files/lib/upgrade/platform.sh
@@ -1,7 +1,7 @@
 PART_NAME=firmware
 REQUIRE_IMAGE_METADATA=1
 
-RAMFS_COPY_BIN='fw_printenv fw_setenv'
+RAMFS_COPY_BIN='fw_printenv fw_setenv fwtool'
 RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
 
 platform_check_image() {
@@ -16,6 +16,7 @@ platform_do_upgrade() {
 		nand_do_upgrade "$1"
 		;;
 	compex,wpq864|\
+	linksys,e8350-v1|\
 	netgear,d7800 |\
 	netgear,r7500 |\
 	netgear,r7500v2 |\
